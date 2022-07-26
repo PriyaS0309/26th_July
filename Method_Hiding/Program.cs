8835e11ca122f5a8a6d8ce44b0d65dec06d0a6e2
PriyaS0309 <109306451+PriyaS0309@users.noreply.github.com>
2022-07-26 13:03:43 +0530

    Add project files.

---
+++ Method_Hiding/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hiding
+{
+
+    class parent                                                         //parent class with show method
+    {
+        
+        public void show()
+        {
+            
+            Console.WriteLine("This is the Parent class method");
+        }
+    }
+    class child:parent                                                 // child class inheriting parent class with the same method name in parent class
+    {
+        public new void show()
+        {
+            Console.WriteLine("This is the child class method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            child c = new child();                              //creating object of child class 
+            c.show();                                           //calls the method of child class only hiding the method of parent class
+
+
+            parent p = new child();                           // but if u want to call the hiden method use this
+            p.show();
+            
+        }
+    }
+}
---
+++ Static_Class/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class
+{
+    static class Product                           //creating static class
+    {
+        static int ProductId;                       // creating static members
+        static string ProductName;
+        static int ProductPrice;
+
+        static Product()                                 //creating static constructors to initialize static members
+        {
+
+            ProductId = 111;
+            Console.WriteLine("Please enter the product name:");
+            ProductName = Console.ReadLine();
+
+            Console.WriteLine("Please enter the product Price:");
+            ProductPrice = int.Parse(Console.ReadLine());
+
+
+        }
+
+       public static void getDetails()                                          //creating static methods
+        {
+            Console.WriteLine("ProductId is:{0}",ProductId);
+            Console.WriteLine("ProductName is:{0}", ProductName);
+            Console.WriteLine("ProductPrice is:{0}", ProductPrice);
+        }
+        
+        public static void getDiscount()
+        {
+
+            int prd_price = ProductPrice / 10;
+            Console.WriteLine("Your discount is:{0}",prd_price);
+            Console.WriteLine("Total amount is:{0}",ProductPrice-prd_price);
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product.getDetails();                                        //accessing the static methods using static class name
+            Product.getDiscount();
+            Console.ReadLine();
+        }
+    }
+}
